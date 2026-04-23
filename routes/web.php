01df4f9f3caa8f127cce507bce9315b6415ba7ef
SeavminhICT <seavminh22220@gmail.com>
2026-04-23 17:19:40 +0700

    add blogs

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('/blog', [PageController::class, 'blog'])->name('blog.index');
+Route::get('/blogs', [PageController::class, 'blog'])->name('blog.index');
 Route::get('/blog/{id}', [PageController::class, 'post'])->name('blog.show');
 Route::get('/experiments', [PageController::class, 'experiments'])->name('experiments');
 Route::get('/about', [PageController::class, 'about'])->name('about');
